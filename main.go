9731a0aac74dca7eb91e442e502b59c49da2e789
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-01-07 14:03:21 +0100

    Set if in command line

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"net"
+	"strings"
 	"time"
 
 	dv_common "github.com/metopa/distributed_variable/common"
@@ -11,7 +13,25 @@ import (
 )
 
 func main() {
-	iface, err := net.InterfaceByName("wlp3s0")
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+	var ifaceNames []string
+	for _, i := range ifaces {
+		ifaceNames = append(ifaceNames, i.Name)
+	}
+
+	ifaceName := flag.String("if", "",
+		"Network interface name. Available: " + strings.Join(ifaceNames, "|"))
+
+	flag.Parse()
+
+	if *ifaceName == "" {
+		panic(fmt.Sprintf("Usage: %s -if <%s>", flag.Arg(0), strings.Join(ifaceNames, "|")))
+	}
+
+	iface, err := net.InterfaceByName(*ifaceName)
 	if err != nil {
 		panic(err)
 	}
