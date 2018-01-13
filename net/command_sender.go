823b31af8f87032cb78f737024717fbdc7d4bfc5
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-01-13 19:45:41 +0100

    Revert old sender code

--- net/command_sender.go
+++ net/command_sender.go
@@ -12,66 +12,6 @@ import (
 const DEFAULT_TTL = 20
 
 func SendToDirectly(ctx *common.Context, destination common.PeerAddr, cmd common.Command) {
-	doSend(ctx, destination, cmd)
-}
-
-func SendToRingLeader(ctx *common.Context, cmd common.Command) {
-	cmd.Destination = ctx.Leader
-	if ctx.LeaderDistance[1] == -1 ||
-		(ctx.LeaderDistance[0] != -1 && ctx.LeaderDistance[0] < ctx.LeaderDistance[1]) {
-		doSendReliable(ctx, ctx.LinkedPeers[0], ctx.LinkedPeers[1], cmd)
-	} else {
-		doSendReliable(ctx, ctx.LinkedPeers[1], ctx.LinkedPeers[0], cmd)
-	}
-}
-
-func SendToHi(ctx *common.Context, cmd common.Command) {
-	addr := ctx.LinkedPeers[1]
-	if len(addr) == 0 {
-		logger.Warn("Hi peer is unknown, send canceled")
-	} else {
-		doSend(ctx, addr, cmd)
-	}
-}
-
-func SendToLo(ctx *common.Context, cmd common.Command) {
-	addr := ctx.LinkedPeers[0]
-	if len(addr) == 0 {
-		logger.Warn("Lo peer is unknown, send canceled")
-	} else {
-		doSend(ctx, addr, cmd)
-	}
-}
-
-func ForwardInRing(ctx *common.Context, from common.PeerAddr, cmd common.Command) {
-	if from == ctx.LinkedPeers[0] {
-		doSendReliable(ctx, ctx.LinkedPeers[1], ctx.LinkedPeers[0], cmd)
-		SendToHi(ctx, cmd)
-	} else if from == ctx.LinkedPeers[1] {
-		doSendReliable(ctx, ctx.LinkedPeers[1], ctx.LinkedPeers[0], cmd)
-	} else {
-		logger.Warn("Tried to forward %v from %v, but linked peers are %v", cmd, from, ctx.LinkedPeers)
-		doSendReliable(ctx, ctx.LinkedPeers[1], ctx.LinkedPeers[0], cmd)
-	}
-}
-
-func ReplyInRing(ctx *common.Context, from common.PeerAddr, cmd common.Command) {
-	if from == ctx.LinkedPeers[0] {
-		doSendReliable(ctx, ctx.LinkedPeers[0], ctx.LinkedPeers[1], cmd)
-	} else if from == ctx.LinkedPeers[1] {
-		doSendReliable(ctx, ctx.LinkedPeers[1], ctx.LinkedPeers[0], cmd)
-	} else {
-		logger.Warn("Tried to reply %v to %v, but linked peers are %v", cmd, from, ctx.LinkedPeers)
-		SendToHi(ctx, cmd)
-	}
-}
-
-func BroadcastInRing(ctx *common.Context, cmd common.Command) {
-	cmd.Destination = "BROADCAST"
-	doSendReliable(ctx, ctx.LinkedPeers[0], ctx.LinkedPeers[1], cmd)
-}
-
-func initServiceFields(ctx *common.Context, destination common.PeerAddr, cmd *common.Command) {
 	cmd.From = ctx.ServerAddr
 	cmd.Clock = ctx.Clock
 	if cmd.Source == "" {
@@ -83,64 +23,86 @@ func initServiceFields(ctx *common.Context, destination common.PeerAddr, cmd *co
 	if cmd.Ttl == 0 {
 		cmd.Ttl = DEFAULT_TTL
 	}
-}
-
-func sendImpl(ctx *common.Context, destination *net.TCPAddr, cmd *common.Command) error {
-	var err error
+	destAddr, err := net.ResolveTCPAddr("tcp", string(destination))
+	if err != nil {
+		logger.Warn("Can't resolve %v: %v", destination, destAddr)
+		return
+	}
 	for i := 0; i < ctx.SendNumRetries; i++ {
-		err = nil
-		conn, err := net.DialTCP("tcp", nil, destination)
+		conn, err := net.DialTCP("tcp", nil, destAddr)
 		if err != nil {
+			logger.Warn("Can't dial %v: %v", destination, err)
 			time.Sleep(ctx.SendRetryPause)
 			continue
 		}
 		e := json.NewEncoder(conn)
 		err = e.Encode(&cmd)
 		if err != nil {
+			logger.Warn("Failed to send data to %v: %v", destination, err)
 			time.Sleep(ctx.SendRetryPause)
 			continue
 		}
-
-		return nil
-	}
-	return err
-}
-
-func doSend(ctx *common.Context, destination common.PeerAddr, cmd common.Command) error {
-	initServiceFields(ctx, destination, &cmd)
-	destAddr, err := net.ResolveTCPAddr("tcp", string(destination))
-	if err != nil {
-		logger.Warn("Can't resolve %v: %v", destination, destAddr)
-		return err
-	}
-
-	err = sendImpl(ctx, destAddr, &cmd)
-
-	if err != nil {
-		logger.Warn("Transmission: %v(%v): Error after %v retries: %v",
-			cmd, common.GetTransmissionInfoString(
-				cmd.Source, ctx.ServerAddr, destination, cmd.Destination),
-			ctx.SendNumRetries, err)
-
-	} else {
-		logger.Info("Transmission: %v(%v): OK", cmd,
+		logger.Info("New transmission: %v: %v", cmd,
 			common.GetTransmissionInfoString(
 				cmd.Source, ctx.ServerAddr, destination, cmd.Destination))
-	}
-
-	return err
-}
-
-func doSendReliable(ctx *common.Context, mainAddr common.PeerAddr,
-	backupAddr common.PeerAddr, cmd common.Command) {
-	err := doSend(ctx, mainAddr, cmd)
-	if err == nil || mainAddr == backupAddr{
 		return
 	}
-	go doSend(ctx, backupAddr, cmd)
-	logger.Warn("Reporting peer %v", mainAddr)
-	report := common.NewReportPeerCommand(mainAddr)
-	report.Destination = ctx.Leader
-	initServiceFields(ctx, backupAddr, &report)
-	doSend(ctx, backupAddr, report)
+
+	logger.Warn("Failed to send data to %v after %v retries", destination, ctx.SendNumRetries)
+}
+
+func SendToRingLeader(ctx *common.Context, cmd common.Command) {
+	cmd.Destination = ctx.Leader
+	if ctx.LeaderDistance[1] == -1 ||
+		(ctx.LeaderDistance[0] != -1 && ctx.LeaderDistance[0] < ctx.LeaderDistance[1]) {
+		SendToLo(ctx, cmd)
+	} else {
+		SendToHi(ctx, cmd)
+	}
+}
+
+func SendToHi(ctx *common.Context, cmd common.Command) {
+	addr := ctx.LinkedPeers[1]
+	if len(addr) == 0 {
+		logger.Warn("Hi peer is unknown, send canceled")
+	} else {
+		SendToDirectly(ctx, addr, cmd)
+	}
+}
+
+func SendToLo(ctx *common.Context, cmd common.Command) {
+	//TODO Send in different direction
+	addr := ctx.LinkedPeers[0]
+	if len(addr) == 0 {
+		logger.Warn("Lo peer is unknown, send canceled")
+	} else {
+		SendToDirectly(ctx, addr, cmd)
+	}
+}
+
+func ForwardInRing(ctx *common.Context, from common.PeerAddr, cmd common.Command) {
+	if from == ctx.LinkedPeers[0] {
+		SendToHi(ctx, cmd)
+	} else if from == ctx.LinkedPeers[1] {
+		SendToLo(ctx, cmd)
+	} else {
+		logger.Warn("Tried to forward %v from %v, but linked peers are %v", cmd, from, ctx.LinkedPeers)
+		SendToHi(ctx, cmd)
+	}
+}
+
+func ReplyInRing(ctx *common.Context, from common.PeerAddr, cmd common.Command) {
+	if from == ctx.LinkedPeers[0] {
+		SendToLo(ctx, cmd)
+	} else if from == ctx.LinkedPeers[1] {
+		SendToHi(ctx, cmd)
+	} else {
+		logger.Warn("Tried to reply %v to %v, but linked peers are %v", cmd, from, ctx.LinkedPeers)
+		SendToHi(ctx, cmd)
+	}
+}
+
+func BroadcastInRing(ctx *common.Context, cmd common.Command) {
+	cmd.Destination = "BROADCAST"
+	SendToHi(ctx, cmd)
 }
